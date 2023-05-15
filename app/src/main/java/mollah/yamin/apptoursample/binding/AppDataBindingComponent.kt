ad180b5f59415a741dc8b622d12d5383e7ad6500
Yamin <myamin335@gmail.com>
2023-05-15 16:37:26 +0200

    Dependencies updated

--- app/src/main/java/mollah/yamin/apptoursample/binding/AppDataBindingComponent.kt
+++ app/src/main/java/mollah/yamin/apptoursample/binding/AppDataBindingComponent.kt
@@ -7,6 +7,5 @@ import androidx.databinding.DataBindingComponent
  */
 class AppDataBindingComponent : DataBindingComponent {
     private val adapter = BindingAdapters()
-
     override fun getBindingAdapters() = adapter
 }
